6dbfae78a22d185f67cb5301bc14f4edbd25c30b
farezaza <110797457+farezaza@users.noreply.github.com>
2023-02-06 13:51:01 +0700

    Q3

---
+++ Q3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double n = double.Parse(Console.ReadLine());
+        Console.WriteLine(f(n));
+    }
+
+    public static double f(double n){
+        if(n >- 1 && n <= 0)
+            {return Math.Sqrt(1-(n * n));}
+        if(n > 0 && n <= 1)
+            {return -Math.Sqrt(1-(n * n));}
+        if(n <= -1)
+            {return f(n + 2);}
+       return f(n - 2);
+    }
+
+}
