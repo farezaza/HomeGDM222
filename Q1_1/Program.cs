88ebd36ca7ade4b7d6d5dee9675c1332f087de56
farezaza <110797457+farezaza@users.noreply.github.com>
2023-02-06 13:43:42 +0700

    question1.1

---
+++ Q1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+
+    public static double f(double x)
+    {
+        if(x * x <= 4) {return 2;}
+       return 1 + f(x-((4*Math.Abs(x))/x));
+    }
+}
